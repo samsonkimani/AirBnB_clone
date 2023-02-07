490419ec31b1ce4e1e7aeb3a896a23e05fdc1476
samsonkimani <samsonkimani43@gmail.com>
2023-02-07 15:09:52 +0300

    updating the intit method

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,21 @@ from datetime import datetime
 
 class BaseModel:
     """ base class for the hbnb """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ initializing the base model varibles"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+            if "created_at" in kwargs:
+                self.created_at = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+
+            if "updated_at" in kwargs:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ function str to return the class name id and dictionary"""

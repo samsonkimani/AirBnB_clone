37d718c8e36ff0add26709d6e982ac3f06a2b533
sagebeme <eudicas5k@gmail.com>
2023-02-08 15:57:47 +0300

    JSON dist, all new save and reload

---
+++ models/engine/filestorage.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" File storage modules"""
+
+import os.path
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage():
+    """ instance of file storage"""
+
+    __files_path = "file.json"
+    __objects = {}
+
+    def __init__(self):
+        """ class constructor """
+        pass
+
+    def all(self):
+        """ returns the dictonary '__objects' """
+
+        return self.__obejcts
+    def new(self, obj):
+        """ sets in __objects the obj with key '<obj class name>.id' """
+        
+        key = obj.__class__.__name__+"."+obj.id
+        self.__object.update({key: obj})
+
+    def save(self):
+        """ serializes __objects to the JSON file (path:file__path) """
+        dict = {}
+        for key in self.__object:
+            dict[key] = self.__object[key].to_dict()
+        with open(self.__file.__path, "w") as f
+            json.dump(dict, f)
+
+    def reload(self):
+        """ deserializes the JSON file to __objects (when JSON (__file_path exists) """
+        try:
+            with open(self.__file_path, r) as f:
+                dict = json.load(f)
+                for i in dict:
+                    self.__objects[i] = class_list[dict[i]["__class__"]](**dict[i])
+        except FileNotFoundError:
+            print("Presistent load failed"
+                    "'{}' do not exist".format(self.__file_path)
+                    )

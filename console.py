3908d55f4747cc8c5871e33ac64f2bebd2e515bf
samsonkimani <samsonkimani43@gmail.com>
2023-02-09 19:57:26 +0300

    creating more classes based on the base model

--- console.py
+++ console.py
@@ -4,10 +4,23 @@
 import cmd
 import models
 from models.base_model import BaseModel
+from models.city import City
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
-classes = {"BaseModel": BaseModel,
-        "User": User}
+classes = {
+        "BaseModel": BaseModel,
+        "User": User,
+        "State": State,
+        "City": City,
+        "Amenity": Amenity,
+        "Place": Place,
+        "Review": Review
+        }
 
 class HBNBCommand(cmd.Cmd):
     """ the command line for the hbnb """

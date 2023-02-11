3382f65bfd99d24d524ea969a624d92247e8876b
Sagebeme <93966131+sagebeme@users.noreply.github.com>
2023-02-11 23:23:22 +0300

    testing HBNBCommant exit bot EOF and quit exits

--- tests/test_console.py
+++ tests/test_console.py
@@ -103,3 +103,15 @@ class TestHBNBCommand_help(unittest.TestCase):
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("help"))
             self.assertEqual(h, output.getvalue().strip())
+
+    
+class TestHBNBCommand_exit(unittest.TestCase):
+    """Unittests for testing exiting from the HBNB command interpreter."""
+
+    def test_quit_exits(self):
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertTrue(HBNBCommand().onecmd("quit"))
+
+    def test_EOF_exits(self):
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertTrue(HBNBCommand().onecmd("EOF"))

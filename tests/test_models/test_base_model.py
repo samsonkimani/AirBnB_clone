ff4afb049e65c69276c5fe0a532933d80d0043cc
sagebeme <eudicas5k@gmail.com>
2023-02-12 02:17:20 +0300

    testing file storage instatiation

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 """module for testing base class"""
 
+import os
+import json
+import models
 from models.base_model import BaseModel
 import unittest
 from datetime import datetime
 import uuid
+from models.engine.file_storage import FileStorage
+from models.user import User
+from models.state import State
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
 
 
 class TestBaseModel(unittest.TestCase):
@@ -73,6 +84,26 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(bm_dict["created_at"], bm.created_at.isoformat())
         self.assertEqual(bm_dict["updated_at"], bm.updated_at.isoformat())
         self.assertEqual(bm_dict["__class__"], "BaseModel")
+
+
+class TestFileStorage_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the FileStorage class."""
+
+    def test_FileStorage_instantiation_no_args(self):
+        self.assertEqual(type(FileStorage()), FileStorage)
+
+    def test_FileStorage_instantiation_with_arg(self):
+        with self.assertRaises(TypeError):
+            FileStorage(None)
+
+    def test_FileStorage_file_path_is_private_str(self):
+        self.assertEqual(str, type(FileStorage._FileStorage__file_path))
+
+    def testFileStorage_objects_is_private_dict(self):
+        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
+
+    def test_storage_initializes(self):
+        self.assertEqual(type(models.storage), FileStorage)
 
 
 if __name__ == '__main__':
